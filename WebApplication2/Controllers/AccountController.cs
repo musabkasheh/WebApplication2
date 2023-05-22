71570703b301783ccb81366b465cbad57b4ef129
musabkasheh <user@MUSAB>
2023-05-23 00:32:24 +0300

    Add project files.

---
+++ WebApplication2/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+using System.Data.SqlClient;
+namespace WebApplication2.Content
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = "data source=MUSAB\\SQLEXPRESS";
+        }
+        public ActionResult verify(Account acc)
+        {
+            return View();
+        }
+    }
+}
